7b52f1ba8321c5ba09c79f018a3ef500020bfe75
Daniel Braddock <Daniel_Braddock@nfumutual.co.uk>
2024-10-04 10:57:45 +0100

    Update to: * docs (desc, params) * change time_stamp to default to FALSE * add ... arg to pass to copy_to * shuffle messages around

--- R/write_sql.R
+++ R/write_sql.R
@@ -1,29 +1,34 @@
 #' write_sql
 #' 
-#' This function is called for it's side-effects of writing a local data.frame to SQL
+#' This function is called for it's side-effects of writing a local data.frame to SQL.
+#' It is essentially a wrapper around dplyr::copy_to with some messages.
+#' The messages log the time the table starts being written, finishes, and how long it took.
 #'
 #' @param df the local data.frame to write to SQL
 #' @param to_conn a remote data source, eg SQL server + database created from DBI::dbConnect
 #' @param to_schema the name of the schema to write to eg "this_schema"
-#' @param to_table the name of the table to write to (needs no datetime stamp) eg "this_table"
-#' @param time_stamp defaults to TRUE, creating a datetime stamp suffix for you table
+#' @param to_table the name of the table to write to eg "this_table" NB a time-stamp is added for you as a suffix if time_stamp = TRUE
+#' @param time_stamp defaults to FALSE, creating a datetime stamp suffix for your table
 #' @param overwrite defaults to TRUE, overwriting any existing table
 #' @param temporary defaults to FALSE, ensuring the table persists after the sessions ends
+#' @param ... args to pass to copy_to
 #'
 #' @return NULL
 #' @export
 #'
 #' @examples \dontrun{write_sql(df, conn, "this_schema", "this_table")}
-write_sql <- function(df, to_conn, to_schema, to_table, time_stamp = TRUE, overwrite = TRUE, temporary = FALSE) {
+write_sql <- function(df, to_conn, to_schema, to_table, time_stamp = FALSE, overwrite = TRUE, temporary = FALSE, ...) {
+  
+  # to pass to messages and use in copy_to
+  to_table_final <- if (time_stamp) paste0(to_table, "_", as_yyyymmdd_hhmmss(t0)) else to_table
+  id_table <- DBI::Id(to_schema, to_table_final)
 
- # log
+  # log
+  message(paste0("Table saving to ", table_name(to_conn, id_table)))
   t0 <- Sys.time()
-  message("================================================================")
   message(paste0(as_yyyymmdd_hhmmss(t0), ": start writing table"))
-  to_table <- if (time_stamp) paste0(to_table, "_", as_yyyymmdd_hhmmss(t0)) else to_table
   
   # write to SQL
-  id_table <- DBI::Id(to_schema, to_table)
   df |> 
     dplyr::copy_to(
       dest = to_conn
@@ -31,6 +36,7 @@ write_sql <- function(df, to_conn, to_schema, to_table, time_stamp = TRUE, overw
       , name = id_table
       , overwrite = overwrite
       , temporary = temporary
+      , ...
     )
   
   # log
@@ -39,6 +45,7 @@ write_sql <- function(df, to_conn, to_schema, to_table, time_stamp = TRUE, overw
   d_write <- (t1 - t0) |> as.numeric(units = "mins") |> round(2)
   message(paste0(d_write, " minutes taken to write table\n"))
   message(paste0("Table saved to ", table_name(to_conn, id_table)))
+  message("================================================================")
   
   return(NULL)
 }
