d3fc02bab2d37cfa70e7799bfe7ff16822448d57
Daniel Braddock <Daniel_Braddock@nfumutual.co.uk>
2024-10-04 15:45:01 +0100

    Add connect_nfu()

---
+++ R/connect_nfu.R
@@ -0,0 +1,39 @@
+#' Connect to database via ODBC
+#' 
+#' A convenience wrapper around DBI::dbConnect(). 
+#' Usefully for Pricing, it defaults to the UWPSQL06 server
+#' 
+#' @details
+#' NB ODBC Data Sources must be set up correctly. 
+#' Basically, add a User DSN for each server. 
+#' I used the driver SQL Server Native Client 11.0. 
+#' Name them after the server for the defaults to work nicely. 
+#' You can use the same name for the Description. 
+#' For example, add a DSN called "UWPSQL06" for the server "UWPSQL06". 
+#'
+#' @param dsn The Data Source Name. A character scalar. 
+#' @param server The server name. A character scalar. 
+#' @param database The database name. A character scalar. 
+#' @param ... 
+#'
+#' @return The output of dbConnect: I expect the class "Microsoft SQL Server"
+#' @export
+#'
+#' @examples
+#' conn <- connect_nfu()
+#' conn_6 <- connect_nfu("UWPSQL06")
+#' conn_4 <- connect_nfu("UWPSQL04")
+#' conn_2 <- connect_nfu("UWPSQL02")
+connect_nfu <- function(dsn = "UWPSQL06"
+                        , server = dsn
+                        , database = "Sandbox"
+                        , ...) {
+  conn <- DBI::dbConnect(
+    drv = odbc::odbc()
+    , dsn = dsn
+    , server = server
+    , database = database
+    , ... = ...
+  )
+  return(conn)
+}
---
+++ tests/testthat/test-connect_nfu.R
@@ -0,0 +1,3 @@
+test_that("connect_nfu with default args does not error", {
+  expect_no_error(connect_nfu())
+})
