f064448e2aff88f58f0479b42e933dda6236fb3c
Daniel Braddock <Daniel_Braddock@nfumutual.co.uk>
2024-10-04 16:55:33 +0100

    update table_name: * don't export * make conn default to connect_nfu()

--- R/table_name.R
+++ R/table_name.R
@@ -3,15 +3,14 @@
 #' By default, gives the fully qualified name with server and database specified
 #' Optionally, if full_name is FALSE, it will just give the schema and table
 #'
-#' @param conn the output of DBI::dbConnect giving the server and db
 #' @param Id the output of DBI::Id containing the schema and table name
+#' @param conn the output of DBI::dbConnect giving the server and db
 #' @param full_name Defaults to TRUE meaning server and db is also given
 #'
 #' @return a <chr> name of the table of the format server.db.schema.table
-#' @export
 #'
 #' @examples \dontrun{table_name(conn, Id)}
-table_name <- function(conn, Id, full_name = TRUE) {
+table_name <- function(Id, conn = connect_nfu(), full_name = TRUE) {
   table_name <- if (full_name) {
     stringr::str_glue("{conn@info$servername}.{conn@info$dbname}.{Id@name[[1]]}.{Id@name[[2]]}")
   } else {
